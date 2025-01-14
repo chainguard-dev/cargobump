e1421f25f76142056ff5eab43a87ada8c52d2a07
hectorj2f <hector@chainguard.dev>
2025-01-14 08:09:09 +0100

    add code to update multiple deps with diff versions
    Signed-off-by: hectorj2f <hector@chainguard.dev>

--- pkg/run/cargo.go
+++ pkg/run/cargo.go
@@ -6,12 +6,13 @@ SPDX-License-Identifier: Apache-2.0
 package run
 
 import (
+	"fmt"
 	"os/exec"
 	"strings"
 )
 
-func CargoUpdatePackage(name, version, cargoRoot string) (string, error) {
-	cmd := exec.Command("cargo", "update", "--precise", version, "--package", name) //nolint:gosec
+func CargoUpdatePackage(name, oldVersion, newVersion, cargoRoot string) (string, error) {
+	cmd := exec.Command("cargo", "update", "--precise", newVersion, "--package", fmt.Sprintf("%s@%s", name, oldVersion)) //nolint:gosec
 	cmd.Dir = cargoRoot
 	if bytes, err := cmd.CombinedOutput(); err != nil {
 		return strings.TrimSpace(string(bytes)), err
