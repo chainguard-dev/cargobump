fdfb2d9e6f9ec75afa7b3338dbef5cc24598af55
Hector Fernandez <hector@chainguard.dev>
2025-01-17 10:10:35 +0100

    add code to update multiple deps with diff versions (#15)
    * add code to update multiple deps with diff versions
    
    Signed-off-by: hectorj2f <hector@chainguard.dev>
    
    * add tests
    
    Signed-off-by: hectorj2f <hector@chainguard.dev>
    
    ---------
    
    Signed-off-by: hectorj2f <hector@chainguard.dev>

--- pkg/update.go
+++ pkg/update.go
@@ -26,13 +26,14 @@ func Update(patches map[string]*types.Package, pkgs []types.CargoPackage, cargoR
 	for _, p := range pkgs {
 		v, exists := patches[p.Name]
 		if exists {
-			log.Printf("Update package: %s\n", p.Name)
-
-			if semver.Compare(p.Version, patches[p.Name].Version) > 0 {
-				return fmt.Errorf("package %s with version '%s' is already at version %s", p.Name, v.Version, p.Version)
+			if semver.Compare("v"+p.Version, "v"+patches[p.Name].Version) > 0 {
+				log.Printf("package %s with version '%s' is already at version %s, skipping it!", p.Name, v.Version, p.Version)
+				continue
 			}
-			if output, err := run.CargoUpdatePackage(p.Name, v.Version, cargoRoot); err != nil {
-				return fmt.Errorf("failed to run cargo update '%v' with error: '%w'", output, err)
+			log.Printf("Update package %s from version %s to %s\n", p.Name, p.Version, v.Version)
+
+			if output, err := run.CargoUpdatePackage(p.Name, p.Version, v.Version, cargoRoot); err != nil {
+				return fmt.Errorf("failed to run cargo update '%v' for package '%s' and version '%s' with error: '%w'", output, p.Name, p.Version, err)
 			}
 
 			log.Printf("Package updated successfully: %s to version %s\n", p.Name, v.Version)
@@ -44,11 +45,11 @@ func Update(patches map[string]*types.Package, pkgs []types.CargoPackage, cargoR
 		if existsVersionRef {
 			log.Printf("Update package with a specific version: %s\n", packageVersion)
 
-			if semver.Compare(p.Version, patches[packageVersion].Version) > 0 {
+			if semver.Compare("v"+p.Version, "v"+patches[packageVersion].Version) > 0 {
 				return fmt.Errorf("warning: package %s with version '%s' is already at version %s", packageVersion, v.Version, p.Version)
 			}
 
-			if output, err := run.CargoUpdatePackage(packageVersion, v.Version, cargoRoot); err != nil {
+			if output, err := run.CargoUpdatePackage(p.Name, p.Version, v.Version, cargoRoot); err != nil {
 				return fmt.Errorf("failed to run cargo update '%v' with error: '%w'", output, err)
 			}
 
